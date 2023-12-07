d989aac9a7883224abef60288fcb0f649d8e2272
ItsJJH <69231931+ItsJJH@users.noreply.github.com>
2023-12-06 19:22:11 -0500

    transmitter code and libraries

---
+++ esp32advertise.py
@@ -0,0 +1,25 @@
+import machine, utime, ubluetooth
+from ble_advertising import advertising_payload as adpl
+
+# Initalize bluetooth and make it run!
+pl = adpl(name='bluetooththingy', customData="stop")
+led = machine.Pin(2, machine.Pin.OUT)
+bt = ubluetooth.BLE()
+bt.active(True)
+
+# Enable flag.
+advertising = True
+try:
+    bt.gap_advertise(0, adv_data=pl, connectable=True)
+except Exception as e:
+    print("Error advertising:", e)
+    advertising = False # Currently, this is the only time when the LED will shut off.
+    
+# Busy loop to keep everything running (and check LED status)!
+while True:
+    if advertising:
+        # Turn LED on
+        led.value(1)
+    else:
+        led.value(0)
+    utime.sleep(10)
