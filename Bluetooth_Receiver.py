7a51f0a2c398bc7f14fb18b17a30da9b28ab0c3f
Prabhjee Singh <prabhjeesingh1@gmail.com>
2023-12-08 15:52:18 -0500

    Updated Bluetooth Receiver
    Implemented uuid check, blink twice rule for the led.

--- Bluetooth_Receiver.py
+++ Bluetooth_Receiver.py
@@ -1,56 +1,86 @@
 import machine, ubluetooth
 from micropython import const
+import time
 
-
-            
+# Constants for Bluetooth scanning
 _IRQ_SCAN_RESULT = const(5)
 _IRQ_SCAN_DONE = const(6)
 
-
+# Function to turn LED on or off
 def turn(val):
-    if val:  # Turn LED on]
-          led.value(1)
-    else:
-            led.value(0)
+    led.value(val)
 
-
-def check_device(adv_data,mac):
-    if(mac == "24:DC:C3:98:B3:9E"):
-        get_readable_data(str(bytearray(adv_data)))
-        print("yay")
+# Function to blink LED twice
+def blink_led():
+    for _ in range(2):
         turn(1)
+        time.sleep(0.5)  # Duration LED is on
+        turn(0)
+        time.sleep(0.5)  # Duration LED is off
+
+# Function to parse advertising data
+def parse_advertising_data(adv_data):
+    i = 0
+    result = {"name": None, "uuids": [], "payload": None}
+    while i + 1 < len(adv_data):
+        length = adv_data[i]
+        if length == 0 or length == 0xFF:
+            break
+        type = adv_data[i + 1]
+        data = adv_data[i + 2:i + length + 1]
+
+        if type == 0x09:  # Complete Local Name
+            result["name"] = bytes(data).decode()
+        elif type in [0x02, 0x03]:  # 16-bit UUIDs
+            for j in range(0, len(data), 2):
+                result["uuids"].append(data[j] + (data[j+1] << 8))
+        elif type == 0xFF:  # Manufacturer Specific Data
+            result["payload"] = data
+
+        i += length + 1
+    return result
 
 
-def get_readable_data(string):
-    split_payload = string.split('\\')
-    name= split_payload[5][1:]
-    payload = split_payload[7][3:-2]
-    print("name=", name)
-    print("Payload=",payload)
-    turn(0)
 
+
+# Function to check if the scanned device matches criteria
+def check_device(adv_data, addr):
+    parsed_data = parse_advertising_data(adv_data)
+    target_uuid = 0x181A  # Target UUID to look for
+
+    # Check for the target UUID and print payload if available
+    if target_uuid in parsed_data["uuids"]:
+        print("Target device found:", addr)
+        payload=bytearray(parsed_data["payload"]).decode('utf-8')
+        print("Payload= ",payload)
+        blink_led()
+        bt.gap_scan(None, 1280000, 11250, True)
+        timer.init(period=10000, mode=machine.Timer.ONE_SHOT, callback=restart_scan)
+
+# Bluetooth interrupt handler
 def bt_irq(event, data):
     if event == _IRQ_SCAN_RESULT:
-        addr_type, addr, adv_type, rssi, adv_data = data   
+        addr_type, addr, adv_type, rssi, adv_data = data
         pop = ':'.join(['%02X' % i for i in addr])
         try:
-            check_device(adv_data,pop)
+            check_device(adv_data, pop)
         except Exception as e:
             print(e)
-      
-        
 
-        
-        
+# Timer callback to restart scan
+def restart_scan(timer):
+    bt.gap_scan(0, 1280000, 11250, True)
+
+# Setup the LED pin
 led = machine.Pin(2, machine.Pin.OUT)
 
-
-# Example usage
-
+# Setup Bluetooth
 bt = ubluetooth.BLE()
 bt.active(True)
 bt.irq(bt_irq)
 bt.gap_scan(0, 1280000, 11250, True)
-turn(0)
+
+# Setup Timer
+timer = machine.Timer(-1)
 
 print("Scanning started.")
